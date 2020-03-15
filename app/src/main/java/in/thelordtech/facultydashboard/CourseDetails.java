15daa4700b6b371ada181db657e1c5f60c1caa20
Dhanishwar R <36957207+Dhanishwar@users.noreply.github.com>
2020-03-16 02:54:28 +0530

    Update CourseDetails.java

--- app/src/main/java/in/thelordtech/facultydashboard/CourseDetails.java
+++ app/src/main/java/in/thelordtech/facultydashboard/CourseDetails.java
@@ -25,7 +25,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/*import com.google.firebase.auth.FirebaseAuth;*/
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
